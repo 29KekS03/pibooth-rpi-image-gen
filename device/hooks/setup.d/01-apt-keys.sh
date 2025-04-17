8d6ff0f3d4a1d0821e5cb4498b02c730648b291d
29KekS03 <45772239+29KekS03@users.noreply.github.com>
2025-04-17 16:53:55 +0200

    update

--- device/hooks/setup.d/01-apt-keys.sh
+++ device/hooks/setup.d/01-apt-keys.sh
@@ -2,10 +2,14 @@
 set -eux
 
 # Debian & RPi GPG Keys
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
-  0E98404D386FA1D9 \
-  6ED0E7B82643E131 \
-  F8D2585B8783D481 \
-  54404762BBB6E853 \
-  BDE6D2B9216EC7A8 \
-  82B129927FA3303E
+- name: Add Missing GPG Keys
+  run: |
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6ED0E7B82643E131
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F8D2585B8783D481
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 54404762BBB6E853
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BDE6D2B9216EC7A8
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 82B129927FA3303E
+
+- name: Run apt-get update
+  run: sudo apt-get update
